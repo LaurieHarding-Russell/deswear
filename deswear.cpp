0f361eccd8a22a98e9c2d5034989b1de7f4b353a
Laurie <laurieHardingRussell@gmail.com>
2018-11-14 21:07:22 -0600

    BROKEN:First attempt. Rebaseing not working as expected.

--- deswear.cpp
+++ deswear.cpp
@@ -8,15 +8,17 @@
 #include "git2.h"
 
 const char * REPO_LOCATION = "/home/laurie/test";
+const char * COMMIT_MESSAGE = "Whoops";
 
 bool containsSwear(const char * summary);
 void deswear(git_repository* repo);
-
+void updateChildren(git_commit *commit);
+void removeSwears(git_repository* repo, git_commit *commit, git_commit *childCommit);
+void rebaseChild(git_repository* repo, git_oid parentCommitId, git_commit *childCommit);
 int main() {
     git_libgit2_init();
 
     git_repository *repo = NULL;
-    git_diff_options diffopts = GIT_DIFF_OPTIONS_INIT;
 
     int error = git_repository_open(&repo, REPO_LOCATION);
     if (error < 0) {    
@@ -36,15 +38,19 @@ void deswear(git_repository* repo) {
     git_revwalk *walker;
     git_revwalk_new(&walker, repo);
     git_revwalk_push_head(walker);
-    
+    git_commit *commit = NULL;
+    git_commit *childCommit = NULL;
     while(!git_revwalk_next(&oid, walker)) {
-        git_commit *commit = NULL;
+        if (commit != NULL){
+            childCommit = commit;
+        }
 		git_commit_lookup(&commit, repo, &oid);
 
         const char * summary = git_commit_summary(commit);
 
         if (containsSwear(summary)) {
             std::cout << git_oid_tostr_s(&oid) << " " << git_commit_summary(commit) << std::endl; 
+            removeSwears(repo, commit, childCommit);
         }
         
 		git_commit_free(commit);
@@ -70,3 +76,37 @@ bool containsSwear(const char * summary) {
     }
     return false;
 }
+
+void removeSwears(git_repository* repo, git_commit *commit, git_commit *childCommit) {
+    git_oid oid;
+    git_commit_amend(&oid, commit, NULL, NULL, NULL, NULL, COMMIT_MESSAGE, NULL);
+    std::cout << "  2. " << git_oid_tostr_s(&oid) << std::endl;
+    if (childCommit != NULL) {
+        rebaseChild(repo, oid, childCommit);
+    }
+}
+
+// Rebasing is yucky.
+void rebaseChild(git_repository* repo, git_oid parentCommitId, git_commit *childCommit) {
+
+    git_annotated_commit *parentAnnotatedCommit = NULL;
+    git_annotated_commit_lookup(&parentAnnotatedCommit, repo, &parentCommitId); 
+
+    const git_oid* childCommitId = git_commit_id(childCommit);
+
+    git_annotated_commit *childAnnotatedCommit;
+    git_annotated_commit_lookup(&childAnnotatedCommit, repo, childCommitId); 
+
+    git_rebase *rebaseObject = NULL;
+    git_rebase_operation *rebaseOperationObject = NULL;
+    if (git_rebase_init(&rebaseObject, repo, childAnnotatedCommit, NULL, parentAnnotatedCommit, NULL) != 0) {
+        std::cout << "We have a problem..." << git_oid_tostr_s(childCommitId) << " \n";
+        exit(1);
+    }
+    if (git_rebase_next(&rebaseOperationObject, rebaseObject) != 0) {
+        std::cout << "We have a next problem... Rebase failed? \n";
+        exit(1);
+    }
+    git_rebase_free(rebaseObject);
+}
+
